0dca829d8d9185c3bd28dea42282a7877ea53dcd
Zainul Alim <za.mi213@gmail.com>
2018-09-10 09:48:27 +0700

    Edit file

--- JAVA/APIWinpay.java
+++ JAVA/APIWinpay.java
@@ -17,7 +17,7 @@ import org.apache.log4j.Logger;
 
 /**
  *
- * @author Zainul Alim
+ * @author Reza Ishaq Maulana <es.mgr@bm.co.id>
  */
 public class APIWinpay {
 
@@ -30,27 +30,33 @@ public class APIWinpay {
         WPIMessage message = new WPIMessage();
         log.info("START");
         message.setCms("API");
-        message.setSpi_amount("5000");
-
-        message.setSpi_billingName("REZA");
-        message.setSpi_billingEmail("rezaishaqm@gmail.com");
-        message.setSpi_billingPhone("085645314075");
+        
+        message.setSpi_billingName("Zainul Alim");
+        message.setSpi_billingEmail("zainulalim@ymail.com");
+        message.setSpi_billingPhone("085649605912");
         message.setSpi_currency("IDR");
-        message.setSkip_spi_page("true");
         message.setSpi_item_expedition("0");
         message.setSpi_token("305626c408a6681dfd39c3a0bc5dea2dfd2f253f8157f7cbe5d72fcdfd962c7d");
-        message.setGet_link("YES");
+        message.setGet_link("no");
 
         List<WPItem> items = new ArrayList<>();
         WPItem wp = new WPItem();
-        wp.setName("BARANG OKE PUNYA");
+        wp.setName("Baju Bali");
         wp.setQty("1");
-        wp.setSku("OLALA");
+        wp.setSku("AG-0189833");
         wp.setUnitPrice("5000");
+        items.add(wp);
+        wp = new WPItem();
+        wp.setName("Baju Jogja");
+        wp.setQty("2");
+        wp.setSku("AB-89898");
+        wp.setUnitPrice("1000");
         items.add(wp);
         message.setSpi_item(items);
         message.setSpi_is_escrow("0");
-
+        
+        message.setSpi_amount("8000");
+        
         Config c = Config.getInstance();
         c.initconfig(Config.DEVELOPMENT);
         WinpayProceed proc = new WinpayProceed(message, PARAMS_PRODUCT_CODE.ALFAMART, c);
